09dfa79cc8c2c17b95bc8ecda0e07af12492af94
AnastasiiaVariuchuk <104990950+AnastasiiaVariuchuk@users.noreply.github.com>
2026-08-20 17:06:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const base = '/nmt-platform/'
+const base = './'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
